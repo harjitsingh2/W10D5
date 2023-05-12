8a43adef10d0d98898d3a1db545d318966a5d236
harjitsingh3 <singh.harjit09@gmail.com>
2023-05-12 15:47:39 -0400

    Finish Phase 0

--- art-museum/src/index.js
+++ art-museum/src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import harvardArt from "./data/harvardArt";
+
+const Root = () => {
+  return (
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+console.log(harvardArt);
